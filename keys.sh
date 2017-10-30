7d47ace25d4b3f15b9dbf2b8f43b3ce061cba333
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2017-10-30 01:04:09 +0100

    Fixed copying of ssh keys

--- keys.sh
+++ keys.sh
@@ -1,20 +1,20 @@
 # Check if a key is already present
 echo "Checking if a key pair is already present..."
 keyAlreadyPresent=$(ls ~/.ssh/ | grep "id_rsa");
-if [ -z "$keyAlreadyPresent" ]; then
+if [ -z $keyAlreadyPresent ]; then
     echo "Key pair not present."
     # Copy key pair from the inserted path
     echo "Type the path of the location of the key pair"
     read keyPath
 
-    if [ -z keyPath ]; then
+    if [ -z $keyPath ]; then
         echo "No path inserted. Skipping."
     else
         echo "Copying keys to ~/.ssh/..."
         echo "Copying private key..."
-        cp $key_path/id_rsa ~/.ssh/
+        cp $keyPath/id_rsa ~/.ssh/
         echo "Copying public key..."
-        cp $key_path/id_rsa.pub ~/.ssh/
+        cp $keyPath/id_rsa.pub ~/.ssh/
     fi
 else
     echo "Key pair already present."
