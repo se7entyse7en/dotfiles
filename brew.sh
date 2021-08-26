35b110433b8dbc84e40e564674d2d09896e74afb
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-27 00:20:05 +0200

    Fix brew installation
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@ echo "Checking if Homebrew already present..."
 if ! which -s brew; then
     # Install Homebrew
     echo "Downloading and installing Homebrew..."
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Make sure we're using the latest Homebrew
