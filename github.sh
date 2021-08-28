b98e7982dc4195ded7fd01b17721d51c53d4dbb5
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 21:53:22 +0200

    Fix location of cloned repositories
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- github.sh
+++ github.sh
@@ -4,10 +4,12 @@ echo "Authenticating with Github token..."
 gh auth login --with-token < ~/.tokens/github/dotfiles.txt
 
 gh_users=(${GH_USERS:-se7entyse7en})
+base_path="/Users/se7entyse7en/Projects/"
 for gh_user in "${gh_users[@]}"
 do
-    echo "Cloning repositories for $gh_user..."
-    mkdir -p "~/Projects/$gh_user" && cd "~/Projects/$gh_user"
+    path="$base_path$gh_user"
+    echo "Cloning repositories for $gh_user in $path..."
+    mkdir -p "$path" && cd "$path"
     gh repo list "$gh_user" --source -L 1000 --json sshUrl --jq '.[] | .sshUrl' | xargs -L1 git clone || true
     echo "All repositories for $gh_user cloned."
 done
