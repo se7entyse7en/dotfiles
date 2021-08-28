fd8bc338c91408291ba9295df513e15e16d31afd
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 19:10:34 +0200

    Make keys installation more robust
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- keys.sh
+++ keys.sh
@@ -1,24 +1,23 @@
 set -e
 
-# Check if a key is already present
-echo "Checking if a key pair is already present..."
+echo "Ensuring existence of ~/.ssh directory..."
 mkdir -p ~/.ssh
-key_already_present=$(ls ~/.ssh/ | grep "id_rsa")
-if [ -z $key_already_present ]; then
-    echo "Key pair not present."
-    # Copy key pair from the inserted path
-    echo "Type the path of the location of the key pair"
-    read key_path
 
-    if [ -z $key_path ]; then
-        echo "No path inserted. Skipping."
-    else
-        echo "Copying keys to ~/.ssh/..."
-        echo "Copying private key..."
-        cp $key_path/id_rsa ~/.ssh/
-        echo "Copying public key..."
-        cp $key_path/id_rsa.pub ~/.ssh/
-    fi
+key_already_present=$(ls ~/.ssh/ | grep "id_rsa")
+if [ -z "$key_already_present" ]; then
+    echo "Key pair not present."
+    while [ -z "$key_path" ]; do
+        echo "Type the path of the location of the key pair"
+        read key_path
+    done
+
+    # Copy key pair from the inserted path
+    echo "Copying keys to ~/.ssh/..."
+    echo "Copying private key..."
+    cp $key_path/id_rsa ~/.ssh/
+    echo "Copying public key..."
+    cp $key_path/id_rsa.pub ~/.ssh/
+
 else
     echo "Key pair already present."
 fi
