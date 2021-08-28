a902b2dddfc70cb79017208b50107c6e3154e7c9
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 19:11:22 +0200

    Extend github.sh script to handle multiple accounts
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- github.sh
+++ github.sh
@@ -3,7 +3,11 @@ set -e
 echo "Authenticating with Github token..."
 gh auth login --with-token < ~/.tokens/github/dotfiles.txt
 
-echo "Cloning repositories for se7entyse7en..."
-mkdir -p ~/Projects/se7entyse7en && cd ~/Projects/se7entyse7en
-gh repo list se7entyse7en --source -L 1000 --json sshUrl --jq '.[] | .sshUrl' | xargs -L1 git clone
-echo "All repositories for se7entyse7en cloned."
+gh_users=(${GH_USERS:-se7entyse7en})
+for gh_user in "${gh_users[@]}"
+do
+    echo "Cloning repositories for $gh_user..."
+    mkdir -p "~/Projects/$gh_user" && cd "~/Projects/$gh_user"
+    gh repo list "$gh_user" --source -L 1000 --json sshUrl --jq '.[] | .sshUrl' | xargs -L1 git clone || true
+    echo "All repositories for $gh_user cloned."
+done
