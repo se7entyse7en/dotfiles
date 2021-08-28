9004e4bcc8a585fe00139a456f1211a6cf5df1d7
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 19:44:10 +0200

    Avoid prompting when installing google cloud SDK components
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- google-cloud-sdk.sh
+++ google-cloud-sdk.sh
@@ -11,4 +11,4 @@ echo "Installing gcloud..."
 /Users/se7entyse7en/google-cloud-sdk/install.sh --rc-path ~/.zshrc --path-update true --command-completion true --bash-completion true --install-python false -q
 
 echo "Installing gcloud components..."
-/Users/se7entyse7en/google-cloud-sdk/bin/gcloud components install core gsutil bq kubectl docker-credential-gcr cloud_sql_proxy
+yes | /Users/se7entyse7en/google-cloud-sdk/bin/gcloud components install core gsutil bq kubectl docker-credential-gcr cloud_sql_proxy
