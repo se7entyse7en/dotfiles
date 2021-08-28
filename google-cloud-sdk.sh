89577a2c6165bec4098ad48cf49b907621bd6d7e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 19:20:09 +0200

    Add missing --path-update flag when installing google cloud SDK
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- google-cloud-sdk.sh
+++ google-cloud-sdk.sh
@@ -8,7 +8,7 @@ curl -LsSo "$tmp_file" "$GCLOUD_SDK_TARBALL_URL" &> /dev/null
 tar -zxf "$tmp_file" -C /tmp
 
 echo "Installing gcloud..."
-/tmp/google-cloud-sdk/install.sh --rc-path ~/.zshrc --command-completion true --bash-completion true --install-python false -q
+/tmp/google-cloud-sdk/install.sh --rc-path ~/.zshrc --path-update --command-completion true --bash-completion true --install-python false -q
 
 echo "Installing gcloud components..."
 gcloud components install core gsutil bq kubectl docker-credential-gcr cloud_sql_proxy
