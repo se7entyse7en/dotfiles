2ece0e772611e29a266008972550bcf681637643
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-08-28 19:41:46 +0200

    Change location for installing google cloud SDK
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- google-cloud-sdk.sh
+++ google-cloud-sdk.sh
@@ -2,13 +2,13 @@ set -e
 
 GCLOUD_SDK_TARBALL_URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-354.0.0-darwin-x86_64.tar.gz"
 
-tmp_file="/tmp/google-cloud-sdk.tar.gz"
-echo "Downloading gcloud into $tmp_file..."
-curl -LsSo "$tmp_file" "$GCLOUD_SDK_TARBALL_URL" &> /dev/null
-tar -zxf "$tmp_file" -C /tmp
+out="/Users/se7entyse7en/google-cloud-sdk.tar.gz"
+echo "Downloading gcloud into $out..."
+curl -LsSo "$out" "$GCLOUD_SDK_TARBALL_URL" &> /dev/null
+tar -zxf "$out" -C /tmp
 
 echo "Installing gcloud..."
-/tmp/google-cloud-sdk/install.sh --rc-path ~/.zshrc --path-update true --command-completion true --bash-completion true --install-python false -q
+/Users/se7entyse7en/google-cloud-sdk/install.sh --rc-path ~/.zshrc --path-update true --command-completion true --bash-completion true --install-python false -q
 
 echo "Installing gcloud components..."
-gcloud components install core gsutil bq kubectl docker-credential-gcr cloud_sql_proxy
+/Users/se7entyse7en/google-cloud-sdk/bin/gcloud components install core gsutil bq kubectl docker-credential-gcr cloud_sql_proxy
